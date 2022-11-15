0b38b902e0c6051aaf82623a52d71962880a8420
unknown <bestairi96@gmail.com>
2022-11-15 08:20:40 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cục đáng yêu';
+const giftUrl = 'https://cucdangyeu.github.io/cucdangyeu/';
+const eventName = 'Happy birthday';
+const titleCard = '';
+const contentCard = 'There’s nothing more precious in this world to me than your sweet smile. On this day, I want to make it even bigger on your face. Happy birthday!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
